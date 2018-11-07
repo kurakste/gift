a836dd217e582e17812be0713e896a3372f5f860
Stepan <kurakste@gmail.com>
2018-11-07 22:48:20 +0300

    Change main menu. Add Url helper. It generate url using controller/method notation.

--- frontend/views/layouts/listashop.php
+++ frontend/views/layouts/listashop.php
@@ -43,7 +43,7 @@ AppAsset::register($this);
                 <nav class="navbar navbar-expand-lg navbar-light main_box">
                     <div class="container">
                         <!-- Brand and toggle get grouped for better mobile display -->
-                        <a class="navbar-brand logo_h" href="#"><img src="/img/logo.png" alt=""></a>
+                        <a class="navbar-brand logo_h" href="<?= Url::toRoute(['site/index']) ?>"><img src="/img/logo.png" alt=""></a>
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
@@ -54,7 +54,10 @@ AppAsset::register($this);
                         <!-- Collect the nav links, forms, and other content for toggling -->
                         <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
                             <ul class="nav navbar-nav menu_nav ml-auto">
-                                <li class="nav-item active"><a class="nav-link" href="#">Главная</a></li>
+                            <li class="nav-item active"><a class="nav-link" href="<?= Url::toRoute(['site/index']) ?>">Главная</a></li>
+                            <li class="nav-item active"><a class="nav-link" href="<?= Url::toRoute(['site/category']) ?>">Выбрать подарок</a></li>
+                            <li class="nav-item active"><a class="nav-link" href="<?= Url::toRoute(['site/category']) ?>">Как это работает</a></li>
+<!--
                                 <li class="nav-item submenu dropdown">
                                     <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Каталог</a>
                                     <ul class="dropdown-menu">
@@ -80,6 +83,7 @@ AppAsset::register($this);
                                                     <li class="nav-item"><a class="nav-link" href="#">Элементы</a></li>
                                         </ul>
                                         </li>
+-->
                                         <li class="nav-item"><a class="nav-link" href="#">Контакты</a></li>
                             </ul>
                             <ul class="nav navbar-nav navbar-right">
