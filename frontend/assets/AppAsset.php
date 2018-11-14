59cd684762910f213b5dc4453c2c79199ce003e0
Stepan <kurakste@gmail.com>
2018-11-14 12:36:59 +0300

    Rebild layout
    Signed-off-by: Stepan <kurakste@gmail.com>

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,25 +12,38 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-//          '/css/site.css',
-        '/css/bootstrap.css',
-        '/css/vendors/linericon/style.css',
-        'css/font-awesome.min.css',
-        '/css/vendors/owl-carousel/owl.carousel.min.css',
-        '/css/vendors/lightbox/simpleLightbox.css',
-        '/css/vendors/nice-select/css/nice-select.css',
-        '/css/vendors/animate-css/animate.css',
-        '/css/vendors/jquery-ui/jquery-ui.css',
-        '/css/style.css',
-        '/css/responsive.css'
+        "/css/bootstrap.css",
+        "/vendors/linericon/style.css",
+        "/css/font-awesome.min.css",
+        "/vendors/owl-carousel/owl.carousel.min.css",
+        "/vendors/lightbox/simpleLightbox.css",
+        "/vendors/nice-select/css/nice-select.css",
+        "/vendors/animate-css/animate.css",
+        "/vendors/jquery-ui/jquery-ui.css", 
+        "/css/style.css",
+        "/css/responsive.css",
     ];
     public $js = [
-        '/js/up.js',
-        '/js/parallax.js',
+
+        "/js/jquery-3.2.1.min.js",
+        "/js/popper.js",
+        "/js/bootstrap.min.js",
+        "/js/stellar.js",
+        "/vendors/lightbox/simpleLightbox.min.js",
+        "/vendors/nice-select/js/jquery.nice-select.min.js",
+        "/vendors/isotope/imagesloaded.pkgd.min.js",
+        "/vendors/isotope/isotope-min.js",
+        "/vendors/owl-carousel/owl.carousel.min.js",
+        "/js/jquery.ajaxchimp.min.js",
+        "/vendors/counter-up/jquery.waypoints.min.js",
+        "/vendors/flipclock/timer.js",
+        "/vendors/counter-up/jquery.counterup.js",
+        "/js/mail-script.js",
+        "/js/theme.js",
     ];
     public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapPluginAsset',
-        '\rmrevin\yii\fontawesome\AssetBundle'
+        /* 'yii\web\YiiAsset', */
+        /* 'yii\bootstrap\BootstrapPluginAsset', */
+        /* '\rmrevin\yii\fontawesome\AssetBundle' */
     ];
 }
