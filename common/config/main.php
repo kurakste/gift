93216df135bd2332e8468de8f3c6ebe38fad05a8
Stepan <kurakste@gmail.com>
2018-12-05 14:04:48 +0300

    Add email parameters

--- common/config/main.php
+++ common/config/main.php
@@ -9,5 +9,19 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        
+        'mailer' =>[ 
+            'class' => 'yii\swiftmailer\Mailer',
+            'transport' => [
+                     'class' => 'Swift_SmtpTransport',
+                     'host' => env('EMAIL_SMTP_HOST'),  // e.g. smtp.mandrillapp.com or smtp.gmail.com
+                     'username' => env('EMAIL_ADDR'),
+                     'password' => env('EMAIL_PASS'),
+                     'port' => env('EMAIL_PORT'), // Port 25 is a very common port too
+                     'encryption' => env('EMAIL_ENC'), // It is often used, check your provider or mail server specs
+                     ],
+
+            'useFileTransport' => false,
+        ],
     ],
 ];
